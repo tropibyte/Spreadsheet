8b08c865a54fd095429283a1777328612279577b
Tarie Nosworthy <mrt303xx@aol.com>
2024-07-22 01:22:02 -0400

    Add project files.

---
+++ Spreadsheet/SpreadsheetDoc.cpp
@@ -0,0 +1,148 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
+// (the "Fluent UI") and is provided only as referential material to supplement the
+// Microsoft Foundation Classes Reference and related electronic documentation
+// included with the MFC C++ library software.
+// License terms to copy, use or distribute the Fluent UI are available separately.
+// To learn more about our Fluent UI licensing program, please visit
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// SpreadsheetDoc.cpp : implementation of the CSpreadsheetDoc class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "Spreadsheet.h"
+#endif
+
+#include "SpreadsheetDoc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CSpreadsheetDoc
+
+IMPLEMENT_DYNCREATE(CSpreadsheetDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CSpreadsheetDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CSpreadsheetDoc construction/destruction
+
+CSpreadsheetDoc::CSpreadsheetDoc() noexcept
+{
+	// TODO: add one-time construction code here
+
+}
+
+CSpreadsheetDoc::~CSpreadsheetDoc()
+{
+}
+
+BOOL CSpreadsheetDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CSpreadsheetDoc serialization
+
+void CSpreadsheetDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Support for thumbnails
+void CSpreadsheetDoc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Modify this code to draw the document's data
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Support for Search Handlers
+void CSpreadsheetDoc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Set search contents from document's data.
+	// The content parts should be separated by ";"
+
+	// For example:  strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CSpreadsheetDoc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CSpreadsheetDoc diagnostics
+
+#ifdef _DEBUG
+void CSpreadsheetDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CSpreadsheetDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CSpreadsheetDoc commands
---
+++ Spreadsheet/SpreadsheetView.cpp
@@ -0,0 +1,138 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface
+// (the "Fluent UI") and is provided only as referential material to supplement the
+// Microsoft Foundation Classes Reference and related electronic documentation
+// included with the MFC C++ library software.
+// License terms to copy, use or distribute the Fluent UI are available separately.
+// To learn more about our Fluent UI licensing program, please visit
+// https://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// SpreadsheetView.cpp : implementation of the CSpreadsheetView class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "Spreadsheet.h"
+#endif
+
+#include "SpreadsheetDoc.h"
+#include "SpreadsheetView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSpreadsheetView
+
+IMPLEMENT_DYNCREATE(CSpreadsheetView, CView)
+
+BEGIN_MESSAGE_MAP(CSpreadsheetView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CSpreadsheetView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CSpreadsheetView construction/destruction
+
+CSpreadsheetView::CSpreadsheetView() noexcept
+{
+	// TODO: add construction code here
+
+}
+
+CSpreadsheetView::~CSpreadsheetView()
+{
+}
+
+BOOL CSpreadsheetView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CSpreadsheetView drawing
+
+void CSpreadsheetView::OnDraw(CDC* /*pDC*/)
+{
+	CSpreadsheetDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+}
+
+
+// CSpreadsheetView printing
+
+
+void CSpreadsheetView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CSpreadsheetView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void CSpreadsheetView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void CSpreadsheetView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void CSpreadsheetView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CSpreadsheetView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CSpreadsheetView diagnostics
+
+#ifdef _DEBUG
+void CSpreadsheetView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSpreadsheetView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSpreadsheetDoc* CSpreadsheetView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSpreadsheetDoc)));
+	return (CSpreadsheetDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CSpreadsheetView message handlers
