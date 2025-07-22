260dfb85fb80d13d4dc639cc7a2dab0215629735
tropibyte <56659150+tropibyte@users.noreply.github.com>
2025-07-22 16:20:59 -0400

    Implement grid management features

--- Spreadsheet/FontRibbonPanel.cpp
+++ Spreadsheet/FontRibbonPanel.cpp
@@ -6,13 +6,30 @@ CFontRibbonPanel::CFontRibbonPanel(LPCTSTR lpszName, HICON hIcon)
 {
 }
 
-void x() {}
 void CFontRibbonPanel::OnLayout()
 {
-    x();
+    CMFCRibbonPanel::OnLayout();
+    int x = m_rect.left + 2;
+    int y = m_rect.top + 2;
+
+    for (int i = 0; i < m_arElements.GetSize(); ++i)
+    {
+        CMFCRibbonBaseElement* pElem = m_arElements[i];
+        if (!pElem)
+            continue;
+        CSize sz = pElem->GetRegularSize(NULL);
+        pElem->SetRect(CRect(x, y, x + sz.cx, y + sz.cy));
+        x += sz.cx + 2;
+    }
 }
 
-void CFontRibbonPanel::OnDraw(CDC* /*pDC*/)
+void CFontRibbonPanel::OnDraw(CDC* pDC)
 {
-    
+    CMFCRibbonPanel::OnDraw(pDC);
+    for (int i = 0; i < m_arElements.GetSize(); ++i)
+    {
+        CMFCRibbonBaseElement* pElem = m_arElements[i];
+        if (pElem)
+            pElem->OnDraw(pDC);
+    }
 }
